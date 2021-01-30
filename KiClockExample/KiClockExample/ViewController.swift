61db6a8e4df9d86f87c65cff7bde9d09f9b329e1
Ivailo Kanev <kanev@mac-mini-di-ivailo.home>
2021-01-30 18:54:44 +0100

    add example

---
+++ KiClockExample/KiClockExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KiClockExample
+//
+//  Created by Ivailo Kanev on 30/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
